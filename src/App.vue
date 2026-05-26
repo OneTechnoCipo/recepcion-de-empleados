ba0cc39ae264dd28cdc2f215b4447c6ea7cac72a
OneTechnoCipo <marianocollazo2003@gmail.com>
2026-05-26 00:29:46 -0300

    feat: create initial project architecture

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-black text-white min-h-screen flex items-center justify-center">
+    <h1 class="text-4xl font-bold">
+      Attendance System
+    </h1>
+  </div>
+</template>
